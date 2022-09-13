1538ba246799d678f3e73fba7c879f9d4b01daaf
akshada <karnikakshada@gmail.com>
2022-09-13 20:07:10 +0530

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,25 @@ class UsersController < ApplicationController
 	def index
 		@users = User.all
 	end
-	def index_stud
-		@students = User.where(role: [0])
+	def stud_profile
+		@current_stud = current_user
 	end
 
 	def show_stud
-    @student = User.find(params[:id]).where(role: [0])
+    #byebug
+    @student = User.find(current_user.id)
   end
-=begin
+
   def edit_stud
-    @user = User.find(params[:id])
+    #byebug
+    @student = User.find(current_user.id)
   end
 
   def update_stud
-    @user = User.find(params[:id])
+    @student = User.find(current_user.id)
 
-    if @user.update(stud_params)
-      redirect_to @user
+    if @student.update(stud_params)
+      redirect_to @student
     else
       render :edit, status: :unprocessable_entity
     end
@@ -26,7 +28,6 @@ class UsersController < ApplicationController
 
   private
   def stud_params
-      params.require(:user).permit(:email, :role)
+      params.require(:user).permit(:email, :role, :status)
   end
-=end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users 
-  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  #devise_scope :user do
-  # get 'sign_in', to: 'devise/sessions#new'
-  #end
-  #devise_for :users, skip: :all
   root 'users#index'
-  get '/stud_profile', to: 'users#index_stud'
-  #get '/stud_show', to: 'users#show_stud'
-  #get '/stud_edit', to: 'users#edit_stud'
-  resources :users
+  get '/stud_profile', to: 'users#stud_profile'
+  get '/show_stud', to: 'users#show_stud'
+  get '/edit_stud', to: 'users#edit_stud'
+  get '/update_stud', to: 'users#update_stud'
+  #resources :users
   resources :colleges
   resources :departments
 end
